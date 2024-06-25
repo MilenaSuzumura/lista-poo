e749ba7669b9e6a0aaeb8ddad95215f6c506baa9
milenasuzumura <milena.suzumura11@gmail.com>
2024-06-25 15:17:49 -0300

    adicionei atributos privados

--- src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
+++ src/main/java/com/trybe/acc/recursoshumanos/PessoaFuncionaria.java
@@ -4,5 +4,8 @@ package com.trybe.acc.recursoshumanos;
  * Classe que representa uma pessoa funcionária no sistema de recursos humanos.
  */
 public class PessoaFuncionaria {
-
+  private String nomeCompleto;
+  private String cpf;
+  private String endereco;
+  private double salario;
 }
