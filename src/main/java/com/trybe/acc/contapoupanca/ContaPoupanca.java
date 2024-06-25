1b82308fadbeb19e10a1fe10edb94fcebcb45a6e
milenasuzumura <milena.suzumura11@gmail.com>
2024-06-25 16:03:18 -0300

    fiz o metodo sacar

--- src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/acc/contapoupanca/ContaPoupanca.java
@@ -8,7 +8,9 @@ public class ContaPoupanca {
     this.saldo = this.saldo + valor;
   }
 
-  // depositar: esse método é responsável por receber um valor do tipo double e somá-lo ao saldo da conta;
+  public void sacar(double valor) {
+    this.saldo = this.saldo - valor;
+  }
   // sacar: esse método é responsável por receber um valor do tipo double e subtraí-lo do saldo da conta;
   // mostrarSaldo: esse método deve retornar um valor do tipo double representando o saldo da conta.
   // mostrarTitularConta: esse método deve retornar o nome da pessoa titular da conta.
